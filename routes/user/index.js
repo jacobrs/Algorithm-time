7571aa0be4a1bc59c26dbd915e0eecefd4abe8b4
Amir Bawab <amirbawab@gmail.com>
2016-05-12 23:29:17 -0400

    Nickname can only be one word

--- routes/user/index.js
+++ routes/user/index.js
@@ -25,15 +25,27 @@ module.exports = function(models) {
 		// Logging
 		console.log("Registration form submitted: " + user);
 
-		// Save new user
-		user.save(function(err) {
-			if(err){
-				viewUtils.load(res, 'user/register', {error_msg: "Operation failed"});
-				console.log("Coudn't save user to DB: " + err);
+		// If no nickname selected
+		if(user.nickname !== undefined && user.nickname.trim() != '') {
+
+			// One word only
+			if(user.nickname.split(' ').length == 1) {
+				// Save new user
+				user.save(function(err) {
+					if(err){
+						viewUtils.load(res, 'user/register', {error_msg: "Couldn't connect to DB. Try again."});
+						console.log("Coudn't save user to DB: " + err);
+					} else {
+						viewUtils.load(res, 'user/register', {success_msg: "Registered successfully"});
+					}
+				});
 			} else {
-				viewUtils.load(res, 'user/register', {success_msg: "Registered successfully"});
+				viewUtils.load(res, 'user/register', {error_msg: "Nickname should not contain spaces"});
 			}
-		});
+			
+		} else {
+			viewUtils.load(res, 'user/register', {error_msg: "Nickname is required"});
+		}
 	});
 
 	return router;
