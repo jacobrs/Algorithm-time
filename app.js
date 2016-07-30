f170bea1ac4e4856cbc7fde6f23ad4b589838f27
Amir Bawab <amirbawab@gmail.com>
2016-07-30 18:28:31 -0400

    Redirect error pages

--- app.js
+++ app.js
@@ -95,10 +95,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: err
-  });
+  res.redirect('/error');
 });
 
 
