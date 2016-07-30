ec400c2623ffbcb75edc67d052562e87ea822199
Amir Bawab <amirbawab@gmail.com>
2016-07-30 14:29:12 -0400

    Fixed update in install page

--- routes/install/index.js
+++ routes/install/index.js
@@ -4,43 +4,50 @@ module.exports = function(models){
 	var viewUtils = require(__base + '/libs/viewUtils');
 	var router = express.Router();
 
-	function adminExists() {
+	function adminExists(res, found, notFound) {
 		var count = 0;
 		models.user_model.find({level: viewUtils.level.ADMIN}, function(err, users) {
-			count = users.length;
+			if(users.length == 0) {
+				notFound(res);
+			} else {
+				found(res);
+			}
 		});
-		return count == 0;
 	}
 
 	router.get('/', function(req, res, next) {
-		console.log(adminExists());
-		if(adminExists()) {
+		adminExists(res, function(res) {
 			res.redirect('/error');
-		} else {
+		}, function(res) {
 			viewUtils.load(res, 'install/index');
-		}
+		});
 	});
 
 	router.post('/', function(req, res, next) {
 
-		if(adminExists()) {
+		adminExists(res, function(res) {
 			res.redirect('/error');
-		} else {
-				models.user_model.findOne({nickname: req.body.nickname}, function(err, user) {
-					if(err) {
-						viewUtils.load(res, 'install/index', {error_msg: "Error connecting to the database. Try again!"});
-					} else {
+		}, function(res) {
+			models.user_model.findOne({nickname: req.body.nickname}, function(err, user) {
+				if(err) {
+					viewUtils.load(res, 'install/index', {error_msg: "Error connecting to the database. Try again!"});
+				} else {
 
-						if(user != null) {
-							console.log(user);
-							models.user_model.update({nickname: req.body.nickname}, {$set: {level: viewUtils.level.ADMIN}}, {});
-							viewUtils.load(res, 'install/index', {success_msg: "Nickname added as admin!"});
-						} else {
-							viewUtils.load(res, 'install/index', {error_msg: "Nickname not found!"});
-						}
-					}	
-				});
-		}
+					if(user != null) {
+						user.level= viewUtils.level.ADMIN;
+						user.save(function(err) {
+							if(err) {
+								viewUtils.load(res, 'install/index', {error_msg: "Error occured. User not promoted."});
+							} else {
+								viewUtils.load(res, 'install/index', {success_msg: "'" + req.body.nickname + "' added as admin!"});
+							}
+						});
+					} else {
+						viewUtils.load(res, 'install/index', {error_msg: "Nickname not found!"});
+					}
+				}	
+			});
+		});
 	});
 
 	return router;
